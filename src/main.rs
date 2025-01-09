46def9865900fe315bc7bb1d463f8b0e3923e4bb
Soulsender <nexushopper3000@gmail.com>
2025-01-08 22:21:04 -0800

    added device/ip iterator function

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,54 @@
+use std::collections::HashMap;
+use regex::Regex;
+use config::Config;
 use telnet::Telnet;
 
-
 fn main() {
-
-    let data = "hello world\nasdasd\n";
+    // basic program configurations
     let server: String = String::from("127.0.0.1");
-    let buffer= data.as_bytes();
+    let port = 4444;
+    let config_path = "config.ini";
+    let config_profiles = "Bookmarks_1";
 
-    use telnet::Telnet;
+    // create item to gather config data from mobaxterm
+    let config = Config::builder()
+        .add_source(config::File::with_name(config_path))
+        .build()
+        .unwrap();
 
-    let mut connection = Telnet::connect((server, 4444), 256)
-            .expect("Couldn't connect to the server...");
+    // returns a hashmap of key/value pairs in the config
+    let config: HashMap<String, String> = config.get(config_profiles).unwrap();
+
+    println!("Found profiles:");
+    let results: Vec<(String, String)> = config
+        .iter()
+        .filter_map(|(key, value)| get_profile(key, value)) // Only keep matches
+        .collect();
+
+    for (device, ip) in results {
+        println!("{} {}", device, ip);
+    }
+
+    
+
+    let mut connection = Telnet::connect((server, port), 256)
+        .expect("Couldn't connect to the server...");
 
     loop {
-        connection.write(buffer).expect("Write Error");
+        // connection.write(data).expect("Write Error");
 
         let event = connection.read().expect("Read Error");
         println!("{:?}", event);
+
     }
 
+}
+
+fn get_profile(key: &str, value: &str) -> Option<(String, String)> {
+    let regex = Regex::new(r"(\b25[0-5]|\b2[0-4][0-9]|\b[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}").unwrap();
+    if let Some(mat) = regex.find(value) {
+        let ip = mat.as_str().trim().to_string();
+        return Some((key.to_string(), ip)); // Return the device and IP as Some
+    }
+    None // Return None if no match is found
 }
